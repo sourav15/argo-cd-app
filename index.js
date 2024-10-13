825eb46227685d208ea6959e55bbe4a6631305f7
Sourav Kodali <souravkodali@reckonsys.com>
2024-10-13 12:34:13 +0530

    update user response to check deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const route = express.Router();
 
 route.get("/user", (req, res) => {
-  res.status(200).send({ id: 1 });
+  res.status(200).send({ id: 2 });
 });
 
 app.use(route);
@@ -13,3 +13,5 @@ app.use(route);
 app.listen(3000, () => {
   console.log("server listening on port 3000");
 });
+
+//argo_cd password: admin@Sourav15
